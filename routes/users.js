a456d06869e3e951a887f7a45f85dda2d4615a1b
Karandeep Singh <karandeepjabbal@gmail.com>
2021-04-09 22:49:45 +0530

    complaints route

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,26 @@ router.get('/login',(req,res)=>{
   res.render('users/login');
 });
 
+router.post('/login',(req,res)=>{
+  
+});
+
 router.get('/dashboard',(req,res)=>{
   res.render('users/dashboard');
+});
+
+router.get('/complaint/:id',(req,res)=>{ // show complaint by id
+    console.log(req.params.id);
+    res.render('users/complaint',{id:req.params.id});
+});
+
+router.get('/complaints',(req,res) =>{   // show user based complaints
+     
+    res.render('users/complaints');
+});
+
+router.post('/complaints',(req,res)=>{
+
 });
 
 module.exports = router;
