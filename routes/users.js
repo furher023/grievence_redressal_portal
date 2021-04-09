a9dd84882adba9598ca3bed26b2bdae76978ba0b
shikher25git <shikher225112@gmail.com>
2021-04-10 02:09:14 +0530

    upd

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var router = express.Router();
+
+express().use(bodyParser.urlencoded({ extended: true }));
+express().use(bodyParser.json())
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,7 +16,9 @@ router.get('/login',(req,res)=>{
 });
 
 router.post('/login',(req,res)=>{
-  
+  console.log(req.body.email);
+  console.log(req.body.pwd);
+  res.send('ok');
 });
 
 router.get('/dashboard',(req,res)=>{
