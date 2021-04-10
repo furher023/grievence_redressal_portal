baae71ea93852d2a50a83d9560fafc77af801256
shikher25git <shikher225112@gmail.com>
2021-04-10 21:17:41 +0530

    multer

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const multer = require("multer");
+const path = require('path');
 var db = require('../modules/dbconnect');
 var router = express.Router();
 
+express().use(bodyParser.json());
+
+const storage = multer.diskStorage({
+  destination: './Uploads/',
+  filename: function (req, file, cb) {
+   cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+ });
+
+ var upload = multer({
+  storage: storage
+ });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -24,13 +38,18 @@ router.get('/complaints',(req,res) =>{   // show user based complaints
     res.render('users/complaints');
 });
 
-router.post('/complaint',(req,res)=>{   // post complaint
+router.post('/complaint', upload.single('proof'), (req,res)=>{   // post complaint
      
-     const complain = new db.model('complaints')(req.body);
-     complain.save();
-     res.json({
-       complain : "Successful"
-     });
+    var reg = '/jpeg|jpg|gif|png/';
+    if (!reg.match(path.extname(req.file.originalname).toLowerCase())){
+      res.send('Please upload an image!')
+    }
+    req.body.proof = './Uploads/' + req.file.fieldname + '-' + Date.now() + path.extname(req.file.originalname);
+    const complain = new db.model('complaints')(req.body);
+    complain.save();
+    res.json({
+      complain : "Successful"
+    });
 });
 
 router.post('/forward',(req,res) => {  // roles allowed        -> hostel secretary
